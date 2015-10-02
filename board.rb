ed7a1a1ae81ecfb8778c82aa89fb562e97a3b2c7
Dave and Pat <daveandpat@appacademy.io>
2015-10-02 12:12:42 -0400

    Added Board structure and bomb positions

--- board.rb
+++ board.rb
@@ -1,5 +1,11 @@
 class Board
 
+  def initialize(dim = [9,9], num_bombs = 6)
+    @dim = dim
+    @grid = Array.new(dim[0]) {Array.new(dim[1])}
+    self.fill
+  end
+
 
   def dimensions
   end
@@ -10,5 +16,17 @@ class Board
   def []
   end
 
+  def fill
 
+  end
+
+  def bomb_pos
+    bomb_pos = []
+    while bomb_pos.count < num_bombs
+      pos = [rand(dim[0]), rand(dim[1])]
+      bomb_pos << pos unless bomb_pos.include?(pos)
+    end
+    bomb_pos
+  end
+  
 end
